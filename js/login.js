e3c5e26fd329fd72ee974a8392c9683244675749
rguamanv-byte <rguamanv@unemi.edu.ec>
2026-06-01 14:36:36 -0500

    Conectar login con backend en Render

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@
 
         try{
 
-            const response = await fetch("http://127.0.0.1:8000/login",{
+            const response = await fetch("https://jugueteria-backend.onrender.com/",{
 
                 method:"POST",
 
