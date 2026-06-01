8754b9fb1e2d5a5fe199b1ed55b33f69b23859c4
rguamanv-byte <rguamanv@unemi.edu.ec>
2026-06-01 15:09:43 -0500

    Actualizar dominio Netlify en CORS

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://TU-PAGINA.netlify.app"
+        "https://g07-juguetelandiaec.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
